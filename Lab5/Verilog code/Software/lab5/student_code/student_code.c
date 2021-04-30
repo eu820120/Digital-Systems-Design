c75d6259523f8b83d69c4dede5d5d9d9f02b369d
eu820120 <60449484+eu820120@users.noreply.github.com>
2021-05-01 00:35:37 +0800

    Add files via upload

---
+++ Lab5/Verilog code/Software/lab5/student_code/student_code.c
@@ -0,0 +1,74 @@
+/*
+ * student_code.c
+ *
+ *  Created on: Mar 7, 2017
+ *      Author: user
+ */
+
+#include <system.h>
+#include <io.h>
+#include "sys/alt_irq.h"
+#include "student_code.h"
+#include "altera_avalon_pio_regs.h"
+
+#ifdef ALT_ENHANCED_INTERRUPT_API_PRESENT
+void handle_lfsr_interrupts(void* context)
+#else
+void handle_lfsr_interrupts(void* context, alt_u32 id)
+#endif
+{
+	#ifdef LFSR_VAL_BASE
+	#ifdef LFSR_CLK_INTERRUPT_GEN_BASE
+	#ifdef DDS_INCREMENT_BASE
+	
+	volatile int* edge_capture_ptr = (volatile int*) context;
+
+
+
+	long int lfsr;
+	lfsr = IORD_ALTERA_AVALON_PIO_DATA(LFSR_VAL_BASE);
+    if ((lfsr%2) == 0) //check if lfsr[0] = 0
+    {
+    	IOWR_ALTERA_AVALON_PIO_DATA(DDS_INCREMENT_BASE, 0x56); //give value to dds_increment
+    }
+    if((lfsr%2) == 1) //check if lfsr[0] = 1
+    {
+    	IOWR_ALTERA_AVALON_PIO_DATA(DDS_INCREMENT_BASE, 0x1AE); //give value to dds_increment
+    }
+
+	*edge_capture_ptr =
+	IORD_ALTERA_AVALON_PIO_EDGE_CAP(LFSR_CLK_INTERRUPT_GEN_BASE);
+	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(LFSR_CLK_INTERRUPT_GEN_BASE, 0);
+	IORD_ALTERA_AVALON_PIO_EDGE_CAP(LFSR_CLK_INTERRUPT_GEN_BASE);
+
+
+
+	#endif
+	#endif
+	#endif
+}
+
+/* Initialize the button_pio. */
+
+void init_lfsr_interrupt()
+{
+	#ifdef LFSR_VAL_BASE
+	#ifdef LFSR_CLK_INTERRUPT_GEN_BASE
+	#ifdef DDS_INCREMENT_BASE
+	
+	/* Enable interrupts */
+	IOWR_ALTERA_AVALON_PIO_IRQ_MASK(LFSR_CLK_INTERRUPT_GEN_BASE, 0x1);
+	/* Reset the edge capture register. */
+	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(LFSR_CLK_INTERRUPT_GEN_BASE, 0x0);
+	/* Register the interrupt handler. */
+#ifdef ALT_ENHANCED_INTERRUPT_API_PRESENT
+	alt_ic_isr_register(LFSR_CLK_INTERRUPT_GEN_IRQ_INTERRUPT_CONTROLLER_ID, LFSR_CLK_INTERRUPT_GEN_IRQ, handle_lfsr_interrupts, 0x0, 0x0);
+#else
+	alt_irq_register( LFSR_CLK_INTERRUPT_GEN_IRQ, NULL,	handle_button_interrupts);
+#endif
+	
+	#endif
+	#endif
+	#endif
+}
+
